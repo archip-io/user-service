435f02de34cbfacdb2184ceb0a04974a83bd4260
ptrvsrg <s.petrov1@g.nsu.ru>
2024-03-08 00:22:17 +0700

    USERS-10: Изменил структуру путей запросов

--- src/main/java/com/archipio/userservice/util/ApiUtils.java
+++ src/main/java/com/archipio/userservice/util/ApiUtils.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApiUtils {
 
-  public static final String SYS_V0_PREFIX = "/sys/v0/users";
+  public static final String SYS_V0_PREFIX = "/sys/v0";
   public static final String SAVE_CREDENTIALS_SUFFIX = "";
   public static final String FIND_CREDENTIALS_SUFFIX = "";
   public static final String RESET_PASSWORD_SUFFIX = "/reset-password";
